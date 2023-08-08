f98b4c5a07a4f3dd0e60a99c1f9470afaf114b0d
ronye <ronye.rocha@gmail.com>
2023-08-07 21:26:42 -0300

    more tests

--- Test/MinMaxSumTests.cs
+++ Test/MinMaxSumTests.cs
@@ -47,5 +47,35 @@ namespace Test
             // Assert
             Assert.Equal(25, result);
         }
+
+        [Fact]
+        public void MaxSum_Returns_Correct_Maximum_Sum_With_Negative_Numbers()
+        {
+            // Arrange
+            List<int> numbers = new List<int> { -5, -9, -3, -2, -8 };
+
+            logger.LogInformation("Sorted list:");
+            numbers.Sort();
+            numbers.ForEach(n => logger.LogInformation(n.ToString()));
+
+            // Act
+            int result = minMaxSum.MaxSum(numbers);
+
+            // Assert
+            Assert.Equal(-25, result);
+        }
+
+        [Fact]
+        public void MaxSum_Returns_Correct_Maximum_Sum_With_Mixed_Numbers()
+        {
+            // Arrange
+            List<int> numbers = new List<int> { -5, 4, -3, 7, -8, 2, 6 };
+
+            // Act
+            int result = minMaxSum.MaxSum(numbers);
+
+            // Assert
+            Assert.Equal(17, result); // The maximum sum is achieved by adding the three largest numbers: 7 + 6 + 4 = 17
+        }
     }
 }
